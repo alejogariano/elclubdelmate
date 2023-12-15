b3c0340616a4e9fba27879d54edc7e5d07eef6a4
alejogariano <alejomatiasgariano@gmail.com>
2023-12-15 20:56:18 -0300

    reiniciar

--- javascript/tienda.js
+++ javascript/tienda.js
@@ -1,78 +1,73 @@
 // Carrito
-
 const carrito = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-    
     fetch('../catalogo.json')
         .then(response => response.json())
         .then(data => mostrarProductos(Object.values(data)))
         .catch(error => console.error('Error al cargar datos:', error));
 
     const calcularCostoTotalBtn = document.getElementById('calcular-costo-total');
+    const mostrarCarritoBtn = document.getElementById('mostrar-carrito');
+
     if (calcularCostoTotalBtn) {
         calcularCostoTotalBtn.addEventListener('click', calcularCostoTotal);
     }
 
-
-    const mostrarCarritoBtn = document.getElementById('mostrar-carrito');
-    const listaCarrito = document.getElementById('lista-carrito');
-
-    mostrarCarritoBtn.addEventListener('click', function () {
-        
-        listaCarrito.style.display = (listaCarrito.style.display === 'none' || listaCarrito.style.display === '') ? 'block' : 'none';
-
-        console.log('Botón clickeado, visibilidad:', listaCarrito.style.display);
-    });
+    if (mostrarCarritoBtn) {
+        mostrarCarritoBtn.addEventListener('click', toggleListaCarrito);
+    }
 });
 
-// funcion para mostrar productos en el DOM
+// Función para mostrar productos en el DOM
 function mostrarProductos(productos) {
     const productosContainer = document.getElementById('productos-container');
 
-
     productos.forEach(producto => {
-
-        const detallesElement = document.createElement('div');
-        detallesElement.classList.add('detalles');
-
-        detallesElement.innerHTML = `
-            <h2>${producto.nombre}</h2>
-            <img src="${producto.imagen}" alt="${producto.nombre}" class="producto-imagen">
-            <p class="material">${producto.material}</p>
-            <p class="precio">${producto.precio}</p>
-            <p class="descripcion">${producto.descripcion}</p>
-            <p class="capacidad">${producto.capacidad}</p>
-            <p class="extraDetalles">${producto.detalles}</p>
-        `;
-
-        const caracteristicasElement = document.createElement('div');
-        caracteristicasElement.classList.add('caracteristicas');
-        producto.caracteristicas.forEach(oracion => {
-            const parrafo = document.createElement('p');
-            parrafo.textContent = oracion;
-            caracteristicasElement.appendChild(parrafo);
-        });
-
-        // boton agregar al carrito
-        const botonAgregar = document.createElement('button');
-        botonAgregar.textContent = 'Agregar al carrito';
-        botonAgregar.addEventListener('click', () => {
-
-            carrito.push(producto);
-            console.log(`Producto "${producto.nombre}" agregado al carrito`);
-            mostrarCarrito();
-        });
-
-        detallesElement.appendChild(caracteristicasElement);
-
-        detallesElement.appendChild(botonAgregar);
-
+        const detallesElement = crearDetallesElement(producto);
         productosContainer.appendChild(detallesElement);
     });
 }
 
-// funcion para mostrar el contenido del carrito
+// Función para crear un elemento de detalles para un producto
+function crearDetallesElement(producto) {
+    const detallesElement = document.createElement('div');
+    detallesElement.classList.add('detalles');
+
+    detallesElement.innerHTML = `
+        <h2>${producto.nombre}</h2>
+        <img src="${producto.imagen}" alt="${producto.nombre}" class="producto-imagen">
+        <p class="material">${producto.material}</p>
+        <p class="precio">${producto.precio}</p>
+        <p class="descripcion">${producto.descripcion}</p>
+        <p class="capacidad">${producto.capacidad}</p>
+        <p class="extraDetalles">${producto.detalles}</p>
+    `;
+
+    const caracteristicasElement = document.createElement('div');
+    caracteristicasElement.classList.add('caracteristicas');
+    producto.caracteristicas.forEach(oracion => {
+        const parrafo = document.createElement('p');
+        parrafo.textContent = oracion;
+        caracteristicasElement.appendChild(parrafo);
+    });
+
+    detallesElement.appendChild(caracteristicasElement);
+
+    const botonAgregar = document.createElement('button');
+    botonAgregar.textContent = 'Agregar al carrito';
+    botonAgregar.addEventListener('click', () => {
+        carrito.push(producto);
+        console.log(`Producto "${producto.nombre}" agregado al carrito`);
+        mostrarCarrito();
+    });
+
+    detallesElement.appendChild(botonAgregar);
+
+    return detallesElement;
+}
+
+// Función para mostrar el contenido del carrito
 function mostrarCarrito() {
     const carritoContainer = document.getElementById('carrito-container');
     const listaCarrito = document.getElementById('lista-carrito');
@@ -98,7 +93,7 @@ function mostrarCarrito() {
     });
 }
 
-// funcion para calcular el costo total y mostrar el carrito
+// Función para calcular el costo total y mostrar el carrito
 function calcularCostoTotal() {
     const mensajeUsuario = document.getElementById("mensaje-usuario");
     const costoTotalElement = document.getElementById("costo-total");
@@ -123,4 +118,10 @@ function calcularCostoTotal() {
     const costoTotalFormateado = costoTotal.toFixed(0);
 
     costoTotalElement.innerText = `El costo total de los productos en el carrito es: $${costoTotalFormateado}`;
+}
+
+// Función para alternar la visibilidad de la lista de carrito
+function toggleListaCarrito() {
+    const listaCarrito = document.getElementById('lista-carrito');
+    listaCarrito.style.display = (listaCarrito.style.display === 'none' || listaCarrito.style.display === '') ? 'block' : 'none';
 }
